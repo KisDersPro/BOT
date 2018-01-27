9adb42ea41cffb7ab1b71108ae41542d771e4c41
KisDersPro <14149074@st.hcmuaf.edu.vn>
2018-01-27 21:11:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const login = require("facebook-chat-api");
 
-login({email: "FB_EMAIL", password: "FB_PASSWORD"}, (err, api) => {
+login({email: "doanbactam.me@gmail.com", password: "tamcoi4"}, (err, api) => {
     if(err) return console.error(err);
 
     fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
